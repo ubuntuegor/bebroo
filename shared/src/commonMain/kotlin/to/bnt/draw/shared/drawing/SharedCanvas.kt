da91bae5e1e5ac6d7def9332877347b13e567351
Egor Kukoverov <me@bnt.to>
2021-12-09 08:03:16 +0300

    shared: DrawingBoard basics

---
+++ shared/src/commonMain/kotlin/to/bnt/draw/shared/drawing/SharedCanvas.kt
@@ -0,0 +1,11 @@
+package to.bnt.draw.shared.drawing
+
+data class Point(val x: Double, val y: Double)
+data class Paint(val fillColor: String = "#000000", val strokeColor: String = "#000000", val strokeWidth: Double = .0)
+
+interface SharedCanvas {
+    val width: Int
+    val height: Int
+    fun bindEvents(board: DrawingBoard)
+    fun drawCircle(center: Point, radius: Double, paint: Paint)
+}
