dcbd2bfce795647e734b90638c8a1ea30eaecf15
Vseslav Kasatskiy <71323710+samorojy@users.noreply.github.com>
2021-12-12 00:22:29 +0300

    update Material Theme (update ColorPalette)

--- app/src/main/java/to/bnt/draw/app/theme/Theme.kt
+++ app/src/main/java/to/bnt/draw/app/theme/Theme.kt
@@ -1,31 +1,24 @@
 package to.bnt.draw.app.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
-private val LightColorPalette = lightColors(
+private val BebrooLightColorPalette = lightColors(
     primary = Coral,
     primaryVariant = Purple700,
-    secondary = Teal200,
-
     background = Color.White,
     surface = Color.White,
     onPrimary = Color.White,
     onSecondary = Color.Black,
     onBackground = Color.Black,
     onSurface = Color.Black,
-
-    )
+)
 
 @Composable
-fun MyApplicationTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable() () -> Unit
-) {
-    val colors = LightColorPalette
+fun BebrooAppTheme(content: @Composable() () -> Unit) {
+    val colors = BebrooLightColorPalette
 
     MaterialTheme(
         colors = colors,
