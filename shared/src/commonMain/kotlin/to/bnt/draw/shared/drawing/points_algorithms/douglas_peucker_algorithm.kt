b4703205e43188daa9229f721dd9dc5d0a5ef8db
mi-sts <66365072+mi-sts@users.noreply.github.com>
2021-12-13 21:30:14 +0300

    Add the Douglas-Peucker algorithm and change file names

---
+++ shared/src/commonMain/kotlin/to/bnt/draw/shared/drawing/points_algorithms/douglas_peucker_algorithm.kt
@@ -0,0 +1,46 @@
+package to.bnt.draw.shared.drawing.points_algorithms
+
+import kotlin.math.abs
+import kotlin.math.sqrt
+
+private fun perpendicularDistance(lineFirstPoint: Point, lineSecondPoint: Point, point: Point): Double {
+    val a = lineSecondPoint.y - lineFirstPoint.y
+    val b = -(lineSecondPoint.x - lineFirstPoint.x)
+    val c = -lineFirstPoint.x * a - lineFirstPoint.y * b
+
+    return abs(a * point.x + b * point.y + c) / sqrt(a*a + b*b)
+}
+
+fun douglasPeuckerAlgorithm(points: List<Point>, epsilon: Double): List<Point> {
+    val indicesStack = ArrayDeque<Pair<Int, Int>>()
+    val keepPoint: Array<Boolean> = Array(points.size) { true }
+    val resultPoints = mutableListOf<Point>()
+    indicesStack.add(Pair(0, points.lastIndex))
+
+    while (!indicesStack.isEmpty()) {
+        val (startIndex, endIndex) = indicesStack.first()
+        indicesStack.removeFirst()
+
+        var maxDistance = 0.0
+        var index = startIndex
+        for (i in (startIndex + 1) until endIndex) {
+            if (keepPoint[i]) {
+                val distance = perpendicularDistance(points[startIndex], points[endIndex], points[i])
+                if (distance > maxDistance) {
+                    index = i
+                    maxDistance = distance
+                }
+            }
+        }
+
+        if (maxDistance >= epsilon) {
+            indicesStack.add(Pair(startIndex, index))
+            indicesStack.add(Pair(index, endIndex))
+        } else {
+            for (i in startIndex + 1 until endIndex)
+                keepPoint[i] = false
+        }
+    }
+
+    return points.filterIndexed { index, _ -> keepPoint[index] }
+}
