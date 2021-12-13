15fe2219fd91f04e232f7d9fd5545993627fbdd8
mi-sts <66365072+mi-sts@users.noreply.github.com>
2021-12-13 20:18:18 +0300

    Fix the first and last points bug

--- shared/src/commonMain/kotlin/to/bnt/draw/shared/drawing/points_algorithms/Point.kt
+++ shared/src/commonMain/kotlin/to/bnt/draw/shared/drawing/points_algorithms/Point.kt
@@ -1,5 +1,10 @@
 package to.bnt.draw.shared.drawing.points_algorithms
 
 data class Point(val x: Double, val y: Double) {
+    operator fun plus(otherPoint: Point) = Point(x + otherPoint.x, y + otherPoint.y)
+    operator fun unaryMinus() = Point(-x, -y)
+    operator fun minus(otherPoint: Point) = plus(-otherPoint)
+
     fun getCoordinates(): Pair<Double, Double> = Pair(x, y)
+    fun getVectorTo(toPoint: Point) = toPoint - this
 }
