8da22ad1097d0b9fe71b6951016aff4e0811e593
Egor Kukoverov <me@bnt.to>
2021-12-13 10:34:25 +0300

    frontend: big update

--- web/src/main/kotlin/to.bnt/bebroo.web/App.kt
+++ web/src/main/kotlin/to.bnt/bebroo.web/App.kt
@@ -1,58 +1,14 @@
 package to.bnt.bebroo.web
 
 import kotlinx.browser.document
-import kotlinx.css.BorderStyle
-import kotlinx.css.Color
-import kotlinx.css.properties.border
-import kotlinx.css.px
-import kotlinx.html.id
 import react.*
 import react.dom.*
 import react.router.dom.BrowserRouter
-import react.router.dom.Link
+import react.router.dom.Redirect
 import react.router.dom.Route
 import react.router.dom.Switch
-import styled.css
-import styled.styledCanvas
-import to.bnt.draw.shared.Platform
-import to.bnt.draw.shared.drawing.DrawingBoard
-import to.bnt.draw.shared.drawing.JsCanvas
-
-val page1 = fc<Props> {
-    h1 {
-        +"page1"
-    }
-    Link {
-        attrs.to = "/"
-        +"go home"
-    }
-}
-
-val canvasPage = fc<PropsWithChildren> {
-    val canvasId = "drawingCanvas"
-    useEffectOnce {
-        val canvas = JsCanvas(canvasId)
-        val board = DrawingBoard(canvas)
-    }
-
-    h1 {
-        +"canvasPage"
-    }
-    styledCanvas {
-        css {
-            border(1.px, BorderStyle.solid, Color.black)
-        }
-        attrs {
-            id = canvasId
-            width = "800"
-            height = "600"
-        }
-    }
-    Link {
-        attrs.to = "/page1"
-        +"go to page 1"
-    }
-}
+import styled.injectGlobal
+import to.bnt.bebroo.web.routes.*
 
 val app = fc<Props> {
     BrowserRouter {
@@ -60,20 +16,29 @@ val app = fc<Props> {
             Route {
                 attrs.path = arrayOf("/")
                 attrs.exact = true
-                canvasPage()
+                Redirect {
+                    attrs.to = "/auth"
+                }
             }
             Route {
-                attrs.path = arrayOf("/page1")
-                page1()
+                attrs.path = arrayOf("/home")
+                homePage()
+            }
+            Route {
+                attrs.path = arrayOf("/auth")
+                authPage()
+            }
+            Route {
+                attrs.path = arrayOf("/board")
+                boardPage()
             }
         }
     }
 }
 
 fun main() {
+    injectGlobal(globalStyles.toString())
     render(document.getElementById("root")) {
         child(app)
     }
 }
-
-fun greet() = "Hello, ${Platform().platform}"
