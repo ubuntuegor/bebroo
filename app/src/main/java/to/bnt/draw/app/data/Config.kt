aa95e271a6cb915c80b038b83c552e165f27f1f1
Vseslav Kasatskiy <71323710+samorojy@users.noreply.github.com>
2021-12-19 19:12:51 +0300

    added to Config APP_URL

--- app/src/main/java/to/bnt/draw/app/data/Config.kt
+++ app/src/main/java/to/bnt/draw/app/data/Config.kt
@@ -1,5 +1,6 @@
 package to.bnt.draw.app.data
 
 object Config {
-    const val url = "https://bebroo.bnt.to/api"
+    const val API_URL = "https://bebroo.bnt.to/api"
+    const val APP_URL = "https://bebroo.bnt.to/"
 }
