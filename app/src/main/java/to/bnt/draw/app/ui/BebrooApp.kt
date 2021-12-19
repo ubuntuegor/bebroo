a4de9db84755e61bd030d9b7b183de3e3a7b8877
Vseslav Kasatskiy <71323710+samorojy@users.noreply.github.com>
2021-12-19 20:16:03 +0300

    added splash screen

--- app/src/main/java/to/bnt/draw/app/ui/BebrooApp.kt
+++ app/src/main/java/to/bnt/draw/app/ui/BebrooApp.kt
@@ -3,18 +3,13 @@ package to.bnt.draw.app.ui
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
-import to.bnt.draw.app.controller.BebrooController
-import to.bnt.draw.app.controller.UserPreferencesManager
-import to.bnt.draw.app.data.SettingsStore
 import to.bnt.draw.app.theme.BebrooAppTheme
 
 //TODO Check values/colors.xml for deleting redundant colors
@@ -27,31 +22,16 @@ fun BebrooApp() {
             SideEffect { systemUiController.setSystemBarsColor(color = Color.White, darkIcons = true) }
         }
 
-        val settingsStoreState = UserPreferencesManager(LocalContext.current).getPreferencesFromDataStore()
-            .collectAsState(initial = SettingsStore())
-        BebrooController.client.token = settingsStoreState.value.token
         val navController = rememberNavController()
-        if (settingsStoreState.value.token == null) {
-            NavHost(navController = navController, startDestination = "login") {
-                composable("login") { LoginScreen(navController) }
-                composable("menu") { MenuScreen(navController) }
-                composable(
-                    route = "board/{id}", arguments = listOf(navArgument("id") { type = NavType.StringType })
-                ) { entry ->
-                    entry.arguments?.getString("id")?.let {
-                        BoardScreen(navController, it)
-                    }
-                }
-            }
-        } else {
-            NavHost(navController = navController, startDestination = "menu") {
-                composable("menu") { MenuScreen(navController) }
-                composable(
-                    route = "board/{id}", arguments = listOf(navArgument("id") { type = NavType.StringType })
-                ) { entry ->
-                    entry.arguments?.getString("id")?.let {
-                        BoardScreen(navController, it)
-                    }
+        NavHost(navController = navController, startDestination = "loading_screen") {
+            composable("loading_screen") { LoadingScreen(navController) }
+            composable("login") { LoginScreen(navController) }
+            composable("menu") { MenuScreen(navController) }
+            composable(
+                route = "board/{id}", arguments = listOf(navArgument("id") { type = NavType.StringType })
+            ) { entry ->
+                entry.arguments?.getString("id")?.let {
+                    BoardScreen(navController, it)
                 }
             }
         }
